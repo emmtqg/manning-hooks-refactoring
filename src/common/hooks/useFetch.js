4c0ae64e3ea97566e303d45d0c20a808e35a2ace
emmtqg <elizabeth.m.mcmahon@gmail.com>
2021-07-19 22:56:14 -0400

    Updated fetch hook to include payload states per Lesson 2 hook implementation

--- src/common/hooks/useFetch.js
+++ src/common/hooks/useFetch.js
@@ -1,7 +1,8 @@
-// from https://www.smashingmagazine.com/2020/07/custom-react-hook-fetch-cache-data/
+// from https://github.com/valentinogagliardi updating
+// my implementation of https://www.smashingmagazine.com/2020/07/custom-react-hook-fetch-cache-data/
 import { useEffect, useRef, useReducer } from 'react';
 
-export const useFetch = (url) => {
+export const useFetch = (endpoint) => {
 	const cache = useRef({});
 
 	const initialState = {
@@ -25,33 +26,43 @@ export const useFetch = (url) => {
 
 	useEffect(() => {
 		let cancelRequest = false;
-		if (!url) return;
+		if (!endpoint) return;
 
 		const fetchData = async () => {
-			dispatch({ type: 'FETCHING' });
-			if (cache.current[url]) {
-				const data = cache.current[url];
+			if (cache.current[endpoint]) {
+				const data = cache.current[endpoint];
 				dispatch({ type: 'FETCHED', payload: data });
-			} else {
-				try {
-					const response = await fetch(url);
-					const data = await response.json();
-					cache.current[url] = data;
-					if (cancelRequest) return;
-					dispatch({ type: 'FETCHED', payload: data });
-				} catch (error) {
-					if (cancelRequest) return;
-					dispatch({ type: 'FETCH_ERROR', payload: error.message });
-				}
-			}
+			} 
+      else {
+        fetch(endpoint)
+        .then(response => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json();
+        })
+        .then(json => {
+          cache.current[endpoint] = json;
+          if (cancelRequest) return;
+          dispatch({ type: "FETCHED", payload: json });
+        })
+        .catch(error => {
+          if (cancelRequest) return;
+          dispatch({ type: "FETCH_ERROR", payload: error.message });
+        })
+        .finally(() => {
+          if (cancelRequest) return;
+          dispatch({ type: "FETCH_FINISH" });
+        });
+      }
 		};
 
 		fetchData();
 
+    // Before we attempt to make state changes, we first confirm if the component has been unmounted. If it has been unmounted, we skip updating the state and if it hasn’t been unmounted, we update the state. This will resolve the React state update error, and also prevent race conditions in our components.
 		return function cleanup() {
 			cancelRequest = true;
 		};
-	}, [url]);
+
+	}, [endpoint]);
 
 	return state;
 };
